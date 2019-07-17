9966458a49f6ffedc63271387f5e73a610613e2f
Mgalanis <Mgalanis@users.noreply.github.com>
2019-07-17 17:49:55 +0300

    RotationFix

---
+++ Armpra/Assets/.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public float horizontalMove;
+    public float verticalMove;
+
+    
+
+    private Rigidbody2D rb;
+    private Transform tf;
+    private Vector2 moveVelocity;
+
+
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+        tf = GetComponent<Transform>();
+    }
+
+    void Update() {
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+        
+    }
+
+    void FixedUpdate() {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+        tf.Rotate(moveVelocity);
+    }
+}
--- Armpra/Assets/Scenes/Gameplay/PlayerMovement.cs
+++ Armpra/Assets/Scenes/Gameplay/PlayerMovement.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
-    public float horizontalMove;
-    public float verticalMove;
-
-    
 
     private Rigidbody2D rb;
     private Transform tf;
     private Vector2 moveVelocity;
 
+    private const float FACE_UP = 0;
+    private const float FACE_UP_LEFT = 45;
+    private const float FACE_LEFT = 90;
+    private const float FACE_DOWN_LEFT = 135;
+    private const float FACE_DOWN = 180;
+    private const float FACE_DOWN_RIGHT = -135;
+    private const float FACE_RIGHT = -90;
+    private const float FACE_UP_RIGHT = -45;
 
+    private float rotationValue;
     void Start(){
         rb = GetComponent<Rigidbody2D>();
         tf = GetComponent<Transform>();
@@ -23,19 +28,26 @@ public class PlayerMovement : MonoBehaviour
     void Update() {
         Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         moveVelocity = moveInput.normalized * speed;
-        
+        if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 1)
+            rotationValue = FACE_UP;
+        else if (Input.GetAxis("Horizontal") == -1 && Input.GetAxis("Vertical") == 1)
+            rotationValue = FACE_UP_LEFT;
+        else if (Input.GetAxis("Horizontal") == -1 && Input.GetAxis("Vertical") == 0)
+            rotationValue = FACE_LEFT;
+        else if (Input.GetAxis("Horizontal") == -1 && Input.GetAxis("Vertical") == -1)
+            rotationValue = FACE_DOWN_LEFT;
+        else if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == -1)
+            rotationValue = FACE_DOWN;
+        else if (Input.GetAxis("Horizontal") == 1 && Input.GetAxis("Vertical") == -1)
+            rotationValue = FACE_DOWN_RIGHT;
+        else if (Input.GetAxis("Horizontal") == 1 && Input.GetAxis("Vertical") == 0)
+            rotationValue = FACE_RIGHT;
+        else if (Input.GetAxis("Horizontal") == 1 && Input.GetAxis("Vertical") == 1)
+            rotationValue = FACE_UP_RIGHT;
     }
 
     void FixedUpdate() {
         rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
-        if (Input.GetAxis("Horizontal") == -1 && Input.GetAxis("Vertical") == -1)
-            tf.Rotate(0,0,(135 - tf.rotation.z) * Time.fixedDeltaTime) ;
-        else if (Input.GetAxis("Horizontal") == -1 && Input.GetAxis("Vertical") == 1)
-            tf.Rotate(0, 0, (45 - tf.rotation.z) * Time.fixedDeltaTime);
-        else if (Input.GetAxis("Horizontal") == 1 && Input.GetAxis("Vertical") == -1)
-            tf.Rotate(0, 0, (-135 - tf.rotation.z) * Time.fixedDeltaTime);
-        else if (Input.GetAxis("Horizontal") == 1 && Input.GetAxis("Vertical") == 1)
-            tf.Rotate(0, 0, (-45 - tf.rotation.z) * Time.fixedDeltaTime);
-        else tf.Rotate(0, 0, 0 * Time.fixedDeltaTime);
+        tf.localRotation = Quaternion.Euler(0, 0, rotationValue);
     }
 }
