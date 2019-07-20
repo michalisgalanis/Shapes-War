afab13addf30a83a07f4ed7ae8647c8c502aa7c3
Mgalanis <Mgalanis@users.noreply.github.com>
2019-07-20 16:03:16 +0300

    Added Pause Menu. Still Incomplete

---
+++ Armpra/Assets/Scenes/Gameplay/Scripts/GameplayManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayManager : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    void Start(){
+        pauseMenu.gameObject.SetActive(false);
+    }
+
+    void Update(){
+        
+    }
+
+    public void Die(){
+
+    }
+}
