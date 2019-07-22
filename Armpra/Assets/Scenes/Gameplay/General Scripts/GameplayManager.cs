1d6d37031a3865f1d470f0c19e35a91f7b45aff3
Mgalanis <Mgalanis@users.noreply.github.com>
2019-07-22 21:38:21 +0300

    Improvements

--- Armpra/Assets/Scenes/Gameplay/General Scripts/GameplayManager.cs
+++ Armpra/Assets/Scenes/Gameplay/General Scripts/GameplayManager.cs
@@ -5,23 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class GameplayManager : MonoBehaviour
 {
+    public GameObject gameUI;
     public GameObject pauseMenu;
     public GameObject lostMenu;
 
     void Start(){
         pauseMenu.gameObject.SetActive(false);
         lostMenu.gameObject.SetActive(false);
+        gameUI.gameObject.SetActive(true);
     }
 
-    void Update(){
-        
+    public void Pause(){
+        Time.timeScale = 0;
+        gameUI.gameObject.SetActive(false);
+        pauseMenu.gameObject.SetActive(true);
+    }
+
+    public void Resume(){
+        Time.timeScale = 1;
+        pauseMenu.gameObject.SetActive(false);
+        gameUI.gameObject.SetActive(true);
     }
 
     public void Lose(){
         lostMenu.gameObject.SetActive(true);
+        Time.timeScale = 0;
     }
 
-    public void GoHome(){
+    public void ReturnHome(){
+        Time.timeScale = 1;
         SceneManager.LoadScene("MainMenu");
         SceneManager.UnloadSceneAsync("Gameplay");
         SceneManager.SetActiveScene(SceneManager.GetSceneByName("MainMenu"));
